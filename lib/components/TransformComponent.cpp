ca4b7b685a678e4ab83d41d97f10f0c448df53ba
Tomasz Ślązak <tomekslego@gmail.com>
2022-01-19 10:59:14 +0100

    improve code readability

--- lib/components/TransformComponent.cpp
+++ lib/components/TransformComponent.cpp
@@ -8,18 +8,29 @@ TransformComponent::TransformComponent(Vector2D& pos, Vector2D& offset): positio
 void TransformComponent::update()
 {
     if (!entity->hasComponent<PlayerComponent>()) {
-        Vector2D* playerPosition = &(GameController::entityManager.getGroup(5)[0])
+        Vector2D* playerPosition
+        = &(GameController::entityManager.getGroup(5)[0])
                 ->getComponent<TransformComponent>().position;
-        float distanceX = std::abs(playerPosition->x - position.x);
-        float distanceY = std::abs(playerPosition->y - position.y);
-        if (playerPosition->x > position.x && distanceX > std::abs(playerPosition->x - (position.x + seamlessOffset.x))) {
+        Vector2D difference = Vector2D(
+                playerPosition->x - position.x,
+                playerPosition->y - position.y
+                );
+        Vector2D distance = Vector2D(
+                std::abs(difference.x),
+                std::abs(difference.y)
+                );
+        if (playerPosition->x > position.x
+        && distance.x > std::abs(difference.x - seamlessOffset.x)) {
             position.x += seamlessOffset.x;
-        } else if (playerPosition->x < position.x && distanceX > std::abs(playerPosition->x - (position.x - seamlessOffset.x))) {
+        } else if (playerPosition->x < position.x
+        && distance.x > std::abs(difference.x + seamlessOffset.x)) {
             position.x -= seamlessOffset.x;
         }
-        if (playerPosition->y > position.y && distanceY > std::abs(playerPosition->y - (position.y + seamlessOffset.y))) {
+        if (playerPosition->y > position.y
+        && distance.y > std::abs(difference.y - seamlessOffset.y)) {
             position.y += seamlessOffset.y;
-        } else if (playerPosition->y < position.y && distanceY > std::abs(playerPosition->y - (position.y - seamlessOffset.y))) {
+        } else if (playerPosition->y < position.y
+        && distance.y > std::abs(difference.y + seamlessOffset.y)) {
             position.y -= seamlessOffset.y;
         }
     }
